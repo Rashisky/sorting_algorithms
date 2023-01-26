bc8b6a23b1598e1285e54abd53a2fadb1e2bd339
Rashisky <rashisky007@gmail.com>
2023-01-26 15:30:42 +0100

    Header file

--- sort.h
+++ sort.h
@@ -23,6 +23,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void sort_list(listint_t **list, listint_t *data_position);
 void print_list(const listint_t *list);
-
+void selection_sort(int *array, size_t size);
 
 #endif
