ccf4316497d1b329a958011e9d28a036a7da9be9
Rashisky <rashisky007@gmail.com>
2023-03-12 16:56:52 +0100

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascening order
+ * selection_sort - sorts an array of integers in ascending order
  * using the "Selection algorithm"
  * @array: array list to be sorted
  * @size: size of the array
@@ -11,38 +11,38 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t count, track, idx = 0;
-	long int val;
-	int i = 1;
 
 	if (array)
 	{
 		while (idx < size) /*replace size*/
 		{
 			track = idx;
-			for (count = idx; count < size; count++)
+			for (count = track; count < size; count++)
 			{
-				if (array[idx] > array[count])
-				{
-					if (i && (array[track] > array[count]))
-					{
-						track = count;
-						i = 0;
-					}
-					else if (!(i) && (array[track] > array[count]))
-					{
-						track = count;
-						i = 1;
-					}
-				}
+				if (array[track] > array[count])
+					track = count;
 			}
 			if (array[idx] > array[track])
 			{
-				val = array[track];
-				array[track] = array[idx];
-				array[idx] = val;
+				swap(array, idx, track);
 				print_array(array, size);
 			}
 			idx++;
 		}
 	}
+}
+
+/**
+ * swap - Interchange the given two values
+ * @array: array list
+ * @i: index position of high_val in the list
+ * @j: index position of low_val in the list
+*/
+void swap(int *array, size_t i, size_t j)
+{
+	long int store;
+
+	store = array[i];
+	array[i] = array[j];
+	array[j] = store;
 }
